38ed953193c477aa06588188e6e1ed57e69e4cae
Alphie Shikwambana <shikwambana27@gmail.com>
2024-05-08 01:51:34 +0200

    moons added but still need to orbit

--- js/script.js
+++ js/script.js
@@ -12,6 +12,8 @@ import uranusTexture from '../img/uranus.jpg'
 import neptuneTexture from '../img/neptune.jpg'
 import saturnTexture from '../img/saturn.jpg'
 import saturnRingTexture from '../img/saturn-ring.png'
+import moonTexture from '../img/moon.jpg'
+
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
@@ -52,57 +54,49 @@ scene.background = cubeTextureLoader.load([
 
 const textuerLoader = new THREE.TextureLoader();
 
-// create saturn's ring
-
-// const saturnRing = new THREE.RingGeometry(10,20,32)
-// const saturnRingMat = new THREE.MeshBasicMaterial({
-//     map: textuerLoader.load(saturnRingTexture),
-//     side: THREE.DoubleSide
-// })
-// const saturnRingMesh = new THREE.Mesh(saturnRing, saturnRingMat)
-// saturnObj.add(saturnRingMesh)
-// saturnRingMesh.position.x = 138
-// saturnRingMesh.position.y = 1.5
-
-
 //create the sun 
 
 const sun = addPlanetToSolarSystem(70, sunTexture, 0);
 
 // create mercury
 
-const mercury = addPlanetToSolarSystem(2, mercuryTexture, 100)
+const mercury = addPlanetToSolarSystem(2, mercuryTexture, 120)
 
 //create venus
 
-const venus = addPlanetToSolarSystem(2.8, venusTexture, 130)
+const venus = addPlanetToSolarSystem(2.8, venusTexture, 145)
 
 
 //create earth
-const earth = addPlanetToSolarSystem(3, earthTexture, 170)
+const earth = addPlanetToSolarSystem(3, earthTexture, 160, 1)
 
 
 //create mars
-const mars = addPlanetToSolarSystem(2.7, marsTexture, 210)
+const mars = addPlanetToSolarSystem(2.7, marsTexture, 180, 2)
 
 // create Jupiter
 
-const jupiter = addPlanetToSolarSystem(7, jupiterTexture, 250)
+const jupiter = addPlanetToSolarSystem(7, jupiterTexture, 210, 10)
 
 //create saturn
 
-const saturn = addPlanetToSolarSystem(5, saturnTexture, 300)
+const saturn = addPlanetToSolarSystem(5, saturnTexture, 270,{
+    innerRadius: 8,
+    outerRadius: 17,
+    texture: saturnRingTexture,
+    position: 270
+})
 
 // create uranus
 
-const uranus = addPlanetToSolarSystem(4, uranusTexture, 330)
+const uranus = addPlanetToSolarSystem(4, uranusTexture, 340, 6)
 
 // create neptune
 
-const neptune = addPlanetToSolarSystem(3.5, marsTexture, 370)
+const neptune = addPlanetToSolarSystem(3.5, neptuneTexture, 400,6)
 
-// add 
-function addPlanetToSolarSystem(size, texture, position, rotationSpeed){
+// add a planet to the solar system
+function addPlanetToSolarSystem(size, texture, position, ringOrMoons){
 
     const planetGeometry = new THREE.SphereGeometry(size, 30 , 30);
     const planetMaterial = new THREE.MeshBasicMaterial({
@@ -113,24 +107,73 @@ function addPlanetToSolarSystem(size, texture, position, rotationSpeed){
 
     const planetObj = new THREE.Object3D();
     planetObj.add(planet);
+
+    if(ringOrMoons && !Number(ringOrMoons)){
+
+        const ringGeometry = new THREE.RingGeometry(
+            ringOrMoons.innerRadius,
+            ringOrMoons.outerRadius,
+            32
+        )
+        const RingMat = new THREE.MeshBasicMaterial({
+            map: textuerLoader.load(ringOrMoons.texture),
+            side: THREE.DoubleSide
+        })
+        const RingMesh = new THREE.Mesh(ringGeometry, RingMat)
+        RingMesh.position.x = ringOrMoons.position
+        RingMesh.position.y = -0.5 * Math.PI
+        planetObj.add(RingMesh)
+
+    }else if(Number(ringOrMoons)){
+        for(let i = 0; i < ringOrMoons; i++){
+            // Calculate a random position for the moon
+            const moonPositionX = position ;
+            const moonPositionY = Math.random() * 20 - 10;
+            const moonPositionZ = Math.random() * 20 - 10;
+
+
+            // add a moon to the planet
+            const moonGeometry = new THREE.SphereGeometry(1, 30, 30);
+            const moonMaterial = new THREE.MeshBasicMaterial({
+                map: textuerLoader.load(moonTexture)
+            });
+            
+            const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+            // Set the moon's position
+            moon.position.set(moonPositionX, moonPositionY, moonPositionZ);
+
+            //add the moon to the planet
+            planetObj.add(moon);
+
+        //     // Create a new object to hold the moon and its orbit
+        //     const moonObj = new THREE.Object3D();
+        //     moonObj.add(moon);
+
+        //    // Set the moon's orbit
+        //     moonObj.position.x = position;
+        //     moonObj.position.y = i * 5;
+
+        //     // Clamp the moon's orbit to a maximum distance of 10 from the planet
+        //     moonObj.position.clampLength(0, 2);
+
+        //     // Add the moon's orbit to the planet
+        //     planetObj.add(moonObj);
+
+        //     // Set a random rotation speed for the moon
+        //     moonObj.rotation.y = Math.random() * 2 * Math.PI;
+
+        }
+
+    }
+
     scene.add(planetObj);
 
     return {planetObj, planet};
 }
 
-// create a ring for a planet 
-function addRingToPlanet(planet, size, texture){
-
-    const ringGeometry = new THREE.RingGeometry(size, size + 1, 32);
-
-
-}
-
-
-
-
 function animate(){
 
+    //rotate around the sun
     sun.planetObj.rotateY(0.001);
     mercury.planetObj.rotateY(0.010);
     venus.planetObj.rotateY(0.005);
@@ -141,6 +184,20 @@ function animate(){
     uranus.planetObj.rotateY(0.0011);
     neptune.planetObj.rotateY(0.0010);
 
+    // for (let i = 0; i < 10; i++) {
+    //     jupiter.planetObj.children[i+1].rotateY(0.001);
+    //   }
+
+    //   for(let i = 0; i < 6; i++){
+    //     uranus.planetObj.children[i+1].rotateY(0.001)
+    //   }
+
+    //   for(let i = 0; i < 6; i++){
+    //     neptune.planetObj.children[i+1].rotateY(0.001)
+    //   }
+
+
+    //rotate around the planet's axis
     sun.planet.rotateY(0.001);
     mercury.planet.rotateY(0.010);
     venus.planet.rotateY(0.005);
